17f56311a8b23683fbac290a2a31f5b0c1b1414e
sanyam83 <64148610+sanyam83@users.noreply.github.com>
2020-08-26 12:16:18 +0530

    Update example.py

--- example_data/example.py
+++ example_data/example.py
@@ -30,7 +30,7 @@ font_cfg = dict(
 perspective_transform = NormPerspectiveTransformCfg(20, 20, 1.5)
 
 chn_data = GeneratorCfg(
-    num_image=2000,
+    num_image=10000,
     save_dir=OUT_DIR / "char_corpus",
     render_cfg=RenderCfg(
         bg_dir=BG_DIR,
@@ -52,7 +52,7 @@ chn_data = GeneratorCfg(
 
 
 rand_data = GeneratorCfg(
-    num_image=2000,
+    num_image=10000,
     save_dir=OUT_DIR / "rand_corpus",
     render_cfg=RenderCfg(
         bg_dir=BG_DIR,
@@ -62,7 +62,7 @@ rand_data = GeneratorCfg(
 )
 
 eng_word_data = GeneratorCfg(
-    num_image=2000,
+    num_image=10000,
     save_dir=OUT_DIR / "word_corpus",
     render_cfg=RenderCfg(
         bg_dir=BG_DIR,
@@ -80,7 +80,7 @@ eng_word_data = GeneratorCfg(
 
 
 same_line_data = GeneratorCfg(
-    num_image=2000,
+    num_image=10000,
     save_dir=OUT_DIR / "same_line_data",
     render_cfg=RenderCfg(
         bg_dir=BG_DIR,
@@ -115,7 +115,7 @@ same_line_data = GeneratorCfg(
 
 
 extra_text_line_data = GeneratorCfg(
-    num_image=2000,
+    num_image=10000,
     save_dir=OUT_DIR / "extra_text_line_data",
     render_cfg=RenderCfg(
         bg_dir=BG_DIR,
