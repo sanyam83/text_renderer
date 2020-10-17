2aa67813e49e22db1010defe84015f12ef41661e
sanyam83 <64148610+sanyam83@users.noreply.github.com>
2020-10-17 12:17:19 +0530

    Update example.py

--- example_data/example.py
+++ example_data/example.py
@@ -29,7 +29,25 @@ font_cfg = dict(
 
 perspective_transform = NormPerspectiveTransformCfg(20, 20, 1.5)
 
-
+chn_data = GeneratorCfg(
+    num_image=10000,
+    save_dir=OUT_DIR / "char_corpus",
+    render_cfg=RenderCfg(
+        bg_dir=BG_DIR,
+        perspective_transform=perspective_transform,
+        corpus=CharCorpus(
+            CharCorpusCfg(
+                text_paths=[TEXT_DIR / "chn_text.txt", TEXT_DIR / "eng_text.txt"],
+                filter_by_chars=True,
+                chars_file=CHAR_DIR / "chn.txt",
+                length=(5, 10),
+                char_spacing=(-0.3, 1.3),
+                **font_cfg
+            ),
+        ),
+        corpus_effects=Effects([Line(0.5), OneOf([DropoutRand(), DropoutVertical()])]),
+    ),
+)
 
 
 rand_data = GeneratorCfg(
@@ -42,13 +60,104 @@ rand_data = GeneratorCfg(
     ),
 )
 
+eng_word_data = GeneratorCfg(
+    num_image=10000,
+    save_dir=OUT_DIR / "word_corpus",
+    render_cfg=RenderCfg(
+        bg_dir=BG_DIR,
+        perspective_transform=perspective_transform,
+        corpus=WordCorpus(
+            WordCorpusCfg(
+                text_paths=[TEXT_DIR / "eng_text.txt"],
+                filter_by_chars=True,
+                chars_file=CHAR_DIR / "eng.txt",
+                **font_cfg
+            ),
+        ),
+    ),
+)
 
+
+same_line_data = GeneratorCfg(
+    num_image=10000,
+    save_dir=OUT_DIR / "same_line_data",
+    render_cfg=RenderCfg(
+        bg_dir=BG_DIR,
+        perspective_transform=perspective_transform,
+        layout=SameLineLayout(),
+        gray=False,
+        corpus=[
+            EnumCorpus(
+                EnumCorpusCfg(
+                    text_paths=[TEXT_DIR / "enum_text.txt"],
+                    filter_by_chars=True,
+                    chars_file=CHAR_DIR / "chn.txt",
+                    **font_cfg
+                ),
+            ),
+            CharCorpus(
+                CharCorpusCfg(
+                    text_paths=[TEXT_DIR / "chn_text.txt", TEXT_DIR / "eng_text.txt"],
+                    filter_by_chars=True,
+                    chars_file=CHAR_DIR / "chn.txt",
+                    length=(5, 10),
+                    font_dir=font_cfg["font_dir"],
+                    font_list_file=font_cfg["font_list_file"],
+                    font_size=(30, 35),
+                ),
+            ),
+        ],
+        corpus_effects=[Effects([Padding(), DropoutRand()]), NoEffects(),],
+        layout_effects=Effects(Line(p=1)),
+    ),
+)
+
+
+extra_text_line_data = GeneratorCfg(
+    num_image=10000,
+    save_dir=OUT_DIR / "extra_text_line_data",
+    render_cfg=RenderCfg(
+        bg_dir=BG_DIR,
+        perspective_transform=perspective_transform,
+        layout=ExtraTextLineLayout(),
+        corpus=[
+            CharCorpus(
+                CharCorpusCfg(
+                    text_paths=[TEXT_DIR / "chn_text.txt", TEXT_DIR / "eng_text.txt"],
+                    filter_by_chars=True,
+                    chars_file=CHAR_DIR / "chn.txt",
+                    length=(9, 10),
+                    font_dir=font_cfg["font_dir"],
+                    font_list_file=font_cfg["font_list_file"],
+                    font_size=(30, 35),
+                ),
+            ),
+            CharCorpus(
+                CharCorpusCfg(
+                    text_paths=[TEXT_DIR / "chn_text.txt", TEXT_DIR / "eng_text.txt"],
+                    filter_by_chars=True,
+                    chars_file=CHAR_DIR / "chn.txt",
+                    length=(9, 10),
+                    font_dir=font_cfg["font_dir"],
+                    font_list_file=font_cfg["font_list_file"],
+                    font_size=(30, 35),
+                ),
+            ),
+        ],
+        corpus_effects=[Effects([Padding()]), NoEffects()],
+        layout_effects=Effects(Line(p=1)),
+    ),
+)
 
 
 
 
 # fmt: off
 configs = [
+    chn_data,
     rand_data,
+    eng_word_data,
+    same_line_data,
+    extra_text_line_data
 ]
 # fmt: on
